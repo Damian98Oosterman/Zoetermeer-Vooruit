9f66ce47afa4e9e92a9a4f24ac5295b86321083e
MegaXLR <admin@megaxlr.net>
2017-03-29 15:09:22 +0200

    Add page,route and controller for create_quizzes
    /quiz/create
    
    this will have a future expand where you can fill in the questions and
    the answers in the create_quizzes route.
    
    Signed-off-by: MegaXLR <admin@megaxlr.net>

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Quiz;
 use Illuminate\Http\Request;
+use App\Http\Requests\QuizRequest;
+use Illuminate\Support\Facades\Redirect;
 
 class QuizController extends Controller
 {
@@ -12,20 +14,23 @@ class QuizController extends Controller
 		return view('forms.quiz.show', ['quizes'=>$quiz->getQuiz()]);
 	}
 
-	public function showQuestions($id) {
-		$data['id']=$id;
-		$quiz=new Quiz();
-		$question=$quiz->showQuestions($data['id']);
+	public function showQuestions( $id ) {
+		$quiz= Quiz::find($id);
+		$question=$quiz->questions;
 		return view('forms.quiz.showquestions', ['questions'=>$question]);
 	}
 
 	public function add() {
-		return view('forms.quiz.add');
+		return view('forms.quiz.add')->with([
+    		'quiz' => Quiz::find(0),
+    	]);
 	}
-	
+
 	public function store(Request $request) {
 		$quiz = new Quiz;
 		$quiz->title = $request->title;
 		$quiz->description = $request->description;
+    	$quiz->save();
+    	return Redirect::to('home')->with('message', __('quiz.message.add.success'));
 	}
 }
