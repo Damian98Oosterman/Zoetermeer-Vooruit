0e97c00dc9f9a7ff2647bff327f05c2e88e32e75
MegaXLR <admin@megaxlr.net>
2017-03-29 10:26:56 +0200

    Stash commit

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,27 +2,30 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
-=======
 use App\Quiz;
->>>>>>> 358ec4eaa4af4443b0124cd7c89398a6f921cedd
 use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
-<<<<<<< HEAD
-    //
-=======
-   public function show(){
-       $quiz=new Quiz();
-       return view('forms.quiz.show', ['quizes'=>$quiz->getQuiz()]);
-   }
+	public function show() {
+		$quiz=new Quiz();
+		return view('forms.quiz.show', ['quizes'=>$quiz->getQuiz()]);
+	}
 
-   public function showQuestions($id){
-       $data['id']=$id;
-       $quiz=new Quiz();
-        $question=$quiz->showQuestions($data['id']);
-       return view('forms.quiz.showquestions', ['questions'=>$question]);
-   }
->>>>>>> 358ec4eaa4af4443b0124cd7c89398a6f921cedd
+	public function showQuestions($id) {
+		$data['id']=$id;
+		$quiz=new Quiz();
+		$question=$quiz->showQuestions($data['id']);
+		return view('forms.quiz.showquestions', ['questions'=>$question]);
+	}
+
+	public function add() {
+		return view('forms.quiz.add');
+	}
+	
+	public function store(Request $request) {
+		$quiz = new Quiz;
+		$quiz->title = $request->title;
+		$quiz->description = $request->description;
+	}
 }
