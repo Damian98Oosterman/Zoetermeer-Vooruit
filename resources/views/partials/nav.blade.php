3a3a57560e5e8a7e3ac2c0ccd847621c91a15be9
Artyssee <martijnmdesigns@hotmail.com>
2017-03-31 13:01:30 +0200

    Added other nav elements
    Added other nav elements

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,8 +1,8 @@
 @section('nav')
 <nav>
   <ul>
-    <li><a href="../welcome.blade.php">Home</a></li>
     <li><a href="../home.blade.php">Home</a></li>
-    <li><a href="../home.blade.php">Home</a></li>
+    <li><a href="../home.blade.php">Test</a></li>
+    <li><a href="../home.blade.php">Test</a></li>
   </ul>
 @endsection
