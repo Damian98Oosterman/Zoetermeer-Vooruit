099750eb859e5efe2d6fed64b5b31b7a10f26c93
Artyssee <martijnmdesigns@hotmail.com>
2017-03-31 12:58:28 +0200

    Control of adding navigation
    Added home navigation

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,5 +1,8 @@
 @section('nav')
-
-NAV
-
+<nav>
+  <ul>
+    <li><a href="../welcome.blade.php">Home</a></li>
+    <li><a href="../home.blade.php">Home</a></li>
+    <li><a href="../home.blade.php">Home</a></li>
+  </ul>
 @endsection
