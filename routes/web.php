4cc402d4b1cbe787aca103544bd61475e3892163
David Mesman <Davidmesman@gmail.com>
2017-04-02 21:05:19 +0200

    Update web.php
    Put "Auth::routes();" under all the groups

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index');
 
 Route::group(['prefix'=>'quiz'], function (){
     Route::get('/', 'QuizController@show');
     Route::get('{id}/questions', 'QuizController@showQuestions');
 });
+
+Auth::routes();
