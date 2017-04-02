f8a4d0c325a926009c7e4ae0ca1e0a5ef5a2cf96
David Mesman <David Mesman>
2017-04-02 15:22:54 +0200

    Add language file for quiz, modified quiz view to match it
    Signed-off-by: David Mesman <David Mesman>

---
+++ resources/lang/nl/quiz.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'table'=>[
+        'columns'=>[
+            'title'=>'Titel',
+            'description'=>'Beschrijving',
+            'startdate'=>'Startdatum',
+            'enddate'=>'Einddatum',
+            'action'=>'Actie',
+        ],
+        'link'=>'Toon vragen',
+    ],
+    'header'=>'Enquetes'
+];
--- resources/views/show/quiz.blade.php
+++ resources/views/show/quiz.blade.php
@@ -5,18 +5,18 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">@lang('show.quiz.nav')</div>
+                    <div class="panel-heading">@lang('quiz.header')</div>
 
                     <div class="panel-body">
                         <table class="table">
                             <thead>
                                 <tr>
                                     <th>Id</th>
-                                    <th>@lang('show.title.column')</th>
-                                    <th>@lang('show.desc.column')</th>
-                                    <th>@lang('show.startdate.column')</th>
-                                    <th>@lang('show.enddate.column')</th>
-                                    <th>@lang('show.action.column')</th>
+                                    <th>@lang('quiz.table.columns.title')</th>
+                                    <th>@lang('quiz.table.columns.description')</th>
+                                    <th>@lang('quiz.table.columns.startdate')</th>
+                                    <th>@lang('quiz.table.columns.enddate')</th>
+                                    <th>@lang('quiz.table.columns.action')</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -27,7 +27,7 @@
                                             <td>{{ $quiz->description }}</td>
                                             <td>{{ $quiz->open_date }}</td>
                                             <td>{{ $quiz->closing_date }}</td>
-                                            <td><a href="/quiz/{{$quiz->id}}/questions">@lang('show.action.name') @lang('show.question.link').'</a></td>
+                                            <td><a href="/quiz/{{$quiz->id}}/questions">@lang('quiz.table.link')</a></td>
                                         </tr>
                                 @endforeach
                             </tbody>
