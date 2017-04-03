6aba2751ee80697fc72b3efb42c9bfe6168f4fa0
Artyssee <martijnmdesigns@hotmail.com>
2017-04-03 15:43:26 +0200

    Updaten login navigation and normal navigation

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -24,20 +24,20 @@
               <ul class="nav navbar-nav navbar-left">
                   @if (Auth::guest())
                       <li><a href="welkom">Welkom</a></li>
+                      <li><a href="about">Over ons</a></li>
                       <li><a href="contact">Contact</a></li>
-                      <li><a href="{{ route('login') }}">Login</a></li>
-                      <li><a href="{{ route('register') }}">Register</a></li>
                   @else
+                    <ul class="nav navbar-nav navbar-left">
+                      <li><a href="home">Home</a></li>
+                      <li><a href="home">Home</a></li>
+                      <li><a href="home">Home</a></li>
+                    </ul>
                       <li class="dropdown">
                           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                               {{ Auth::user()->name }} <span class="caret"></span>
                           </a>
-
                           <ul class="dropdown-menu" role="menu">
-                              <li>
-                                  <a href="{{ route('logout') }}"
-                                      onclick="event.preventDefault();
-                                               document.getElementById('logout-form').submit();">
+                              <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                       Logout
                                   </a>
 
@@ -49,8 +49,16 @@
                       </li>
                   @endif
               </ul>
+              <ul class="nav navbar-nav navbar-right">
+                  @if (Auth::guest())
+                      <li><a href="{{ route('login') }}">Login</a></li>
+                      <li><a href="{{ route('register') }}">Register</a></li>
+                  @else
+
+                  @endif
+              </ul>
           </div>
       </div>
   </nav>
-
 @endsection
+<div class="navspace"></div>
