84ed2cb1618ca79eb76af1eb8811a7c6c820a9e8
Damian Oosterman <damian.oosterman@gmail.com>
2017-04-05 09:48:57 +0200

    add random header image on background
    Signed-off-by: Damian Oosterman <damian.oosterman@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,25 @@
 @extends('layouts.app')
 @section('content')
   <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  
     <script>tinymce.init({ selector:'textarea' });</script>
+    <script>
+    $(document).ready(function(){
+        var images=["{{ asset('img/background/01.jpg') }}",
+                    "{{ asset('img/background/02.jpg') }}",
+                    "{{ asset('img/background/03.jpg') }}",
+                    "{{ asset('img/background/04.jpg') }}",];
+
+            var randomNumber = Math.floor(Math.random() * images.length);
+            var bgImg = 'url(' + images[randomNumber] + '';
+
+            $('header').css({'background-image':bgImg, 'background-size':'cover', });
+            console.log(bgImg);
+            console.log(randomNumber);
+    });
+
+    </script>
 
 <header>
   <div class="dark">
@@ -11,7 +29,7 @@
   </div>
 </header>
 <section class="color-white">
-  <div class="">
+  <div class="container">
       <br>
       <textarea>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
   </div>
