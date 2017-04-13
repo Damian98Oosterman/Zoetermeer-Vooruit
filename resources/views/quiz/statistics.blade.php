fb516da9ae579f84df1305f5372474a82b0a5ef9
Damian Oosterman <damian.oosterman@gmail.com>
2017-04-13 11:36:23 +0200

    deleted unnessecary files and fixed bugs in statistics page where footer got displayed wrong
    Signed-off-by: Damian Oosterman <damian.oosterman@gmail.com>

--- resources/views/quiz/statistics.blade.php
+++ resources/views/quiz/statistics.blade.php
@@ -3,6 +3,7 @@
   <script src="{{asset("bower_components/chart.js/dist/Chart.min.js")}}" charset="utf-8"></script>
 
       <div class="container" id="chart">
+        <h2>{{$quiz}}</h2>
       </div>
 
 @endsection
