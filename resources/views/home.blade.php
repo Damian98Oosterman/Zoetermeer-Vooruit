8b6d118706c10b467a1aeeb9b1575746424b8b92
Damian Oosterman <damian.oosterman@gmail.com>
2017-05-08 13:12:09 +0200

    created base design for the dashboardpage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,11 +11,20 @@
     	@endif
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading"><h4>@lang('dashboard.user.header')</h4></div>
 
                 <div class="panel-body">
-                    This is empty for now.
+                  <h3>@lang('dashboard.user.quiz')</h3>
+                  <div class="panel panel-default">
+                    <div class="col-md-8 col-sm-4 col-xs-12">
+                    <h5>[QUIZ_TITLE_PLACEHOLDER]</h5>
+                    <small>[QUIZ_DESCRIPTION_PLACEHOLDER]</small>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-10">
+                    <button class="btn pull-right" type="button" name="button" style="margin-right: 25%">Maak Quiz</button>
+                    </div>
                 </div>
+              </div>
             </div>
         </div>
     </div>
