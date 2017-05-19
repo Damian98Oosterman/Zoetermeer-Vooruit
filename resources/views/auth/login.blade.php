4ea9b11ad7d039ba358092b641a768e411bcac52
Artyssee <martijnmdesigns@hotmail.com>
2017-05-19 09:30:48 +0200

    Added Placeholders

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@ body{
                             <!-- <label for="email" class="col-md-4 control-label">E-Mail Address</label> -->
 
                             <div class="col-md-10 col-md-offset-1">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" placeholder="E-mail adres" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -37,7 +37,7 @@ body{
                             <!-- <label for="password" class="col-md-4 control-label">Password</label> -->
 
                             <div class="col-md-10 col-md-offset-1">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" placeholder="Password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
