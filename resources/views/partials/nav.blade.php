9724aae1acdf02df2e79e0d2606cd0b3618412df
Artyssee <martijnmdesigns@hotmail.com>
2017-05-19 12:58:02 +0200

    Simply added Register page to navigation

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -52,6 +52,7 @@
                   @if (Auth::guest())
                   <ul class="nav navbar-nav navbar-right">
                       <li><a href="{{ route('login') }}">Login</a></li>
+                      <li><a href="{{ route('register') }}">Register</a></li>
                   </ul>
                   @else
 
